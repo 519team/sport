ffc8a1e09ce5bb5b77ef7e6c4c749bb0ce4326f1
519team <68482686+519team@users.noreply.github.com>
2020-07-19 17:50:01 +0300

    Add files via upload

--- SPORT/views.py
+++ SPORT/views.py
@@ -4,8 +4,9 @@ from .models import Category
 
 
 def site_lists(request):
-	return render(request,'base.html')
+	categorys=Category.objects.all()
+	return render(request,'base.html', context={'categorys': categorys})
 
 def category_detail(request, index_cat):
 	categorys=Category.objects.all()
-	return render(request, 'base.html', context={'categorys': categorys})
+	return render(request, 'SPORT/category.html', context={'categorys': categorys})
