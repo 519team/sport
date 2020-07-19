cabca67098931693516b2509314855097bbba936
519team <68482686+519team@users.noreply.github.com>
2020-07-19 02:11:48 -0700

    Add files via upload

--- SPORT/views.py
+++ SPORT/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category
 
 
 def site_lists(request):
 	return render(request,'base.html')
 
 def category_detail(request, index_cat):
-	category=Category.objects.get(index_cat__iexact=index_cat)
-	return render(request, 'SPORT/category.html', context={'category': category})
+	categorys=Category.objects.all()
+	return render(request, 'base.html', context={'categorys': categorys})
