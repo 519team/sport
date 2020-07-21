98bc5f619af3e0f34c2ba5473466f4dce7dd8072
519team <68482686+519team@users.noreply.github.com>
2020-07-21 19:40:19 +0300

    Add files via upload

--- SPORT/urls.py
+++ SPORT/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns=[
 	path('',site_lists, name='home_page'),
-	path('shop/category/<int:index_cat>/', category_detail, name='cat_url')
+	path('shop/category/<int:index_cat>/', category_detail, name='cat_url'),
+	path('shop/item/<int:index_item>/', item_detail, name='item_url')
 ]
--- SPORT/views.py
+++ SPORT/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category
-
+from .models import Item
 
 def site_lists(request):
 	categorys=Category.objects.all()
 	return render(request,'base.html', context={'categorys': categorys})
 
 def category_detail(request, index_cat):
+	Categorys=Category.objects.all()
+	category=Category.objects.get(index_cat=index_cat)
+	items=Item.objects.filter(id_category=category.id)
+	return render(request, 'SPORT/category.html', context={'category': category, 'items': items, 'categorys': Categorys})
+
+def item_detail(request, index_item):
 	categorys=Category.objects.all()
-	return render(request, 'SPORT/category.html', context={'categorys': categorys})
+	return render(request, 'SPORT/item.html', context={'categorys': categorys})
